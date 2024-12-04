679a286ba1478791f4befd18d7e2434637318243
StuOnGit <destasiofrancesco2000@gmail.com>
2024-12-04 15:43:35 +0100

    US2 - Green Phase

--- src/cleaning_robot.py
+++ src/cleaning_robot.py
@@ -81,8 +81,17 @@ class CleaningRobot:
         pass
 
     def manage_cleaning_system(self) -> None:
-        # To be implemented
-        pass
+        if self.ibs.get_charge_left() <= 10:
+            GPIO.output(self.RECHARGE_LED_PIN, GPIO.HIGH)
+            GPIO.output(self.CLEANING_SYSTEM_PIN, GPIO.LOW)
+            self.recharge_led_on = True
+            self.cleaning_system_on = False
+        elif 100 >= self.ibs.get_charge_left() > 10:
+            GPIO.output(self.CLEANING_SYSTEM_PIN, GPIO.HIGH)
+            GPIO.output(self.CLEANING_SYSTEM_PIN, GPIO.LOW)
+            self.cleaning_system_on = True
+            self.recharge_led_on = False
+
 
     def activate_wheel_motor(self) -> None:
         """
